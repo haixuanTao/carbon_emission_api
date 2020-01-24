ba80eb407f22d9ac4a6fe96b59bee33e63e99dd9
Haixuan Tao <hai-xuan.tao@student.ecp.fr>
2020-01-24 11:58:31 +0100

    allowing CORS

--- instagram_carbon_emission/app.py
+++ instagram_carbon_emission/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask.ext.cors import CORS, cross_origin
 
 from instagram_carbon_emission import auth, api
 from instagram_carbon_emission.extensions import db, jwt, migrate, apispec
@@ -7,12 +8,26 @@ from instagram_carbon_emission.extensions import db, jwt, migrate, apispec
 def create_app(testing=False, cli=False):
     """Application factory, used to create application
     """
-    app = Flask('instagram_carbon_emission', static_folder="./static")
-    app.config.from_object('instagram_carbon_emission.config')
+    app = Flask("instagram_carbon_emission", static_folder="./static")
+    app.config.from_object("instagram_carbon_emission.config")
 
     if testing is True:
-        app.config['TESTING'] = True
+        app.config["TESTING"] = True
 
+    CORS(
+        app,
+        resources={
+            r"/api/": {
+                "origins": "https://instagram-carbon-emission.s3.eu-west-3.amazonaws.com/index.html",
+                "allow_headers": [
+                    "Content-Type",
+                    "Authorization",
+                    "Access-Control-Allow-Credentials",
+                ],
+                "supports_credentials": True,
+            }
+        },
+    )
     configure_extensions(app, cli)
     configure_apispec(app)
     register_blueprints(app)
@@ -34,19 +49,20 @@ def configure_apispec(app):
     """Configure APISpec for swagger support
     """
     apispec.init_app(app, security=[{"jwt": []}])
-    apispec.spec.components.security_scheme("jwt", {
-        "type": "http",
-        "scheme": "bearer",
-        "bearerFormat": "JWT",
-    })
+    apispec.spec.components.security_scheme(
+        "jwt", {"type": "http", "scheme": "bearer", "bearerFormat": "JWT",}
+    )
     apispec.spec.components.schema(
-        "PaginatedResult", {
+        "PaginatedResult",
+        {
             "properties": {
                 "total": {"type": "integer"},
                 "pages": {"type": "integer"},
                 "next": {"type": "string"},
                 "prev": {"type": "string"},
-            }})
+            }
+        },
+    )
 
 
 def register_blueprints(app):
